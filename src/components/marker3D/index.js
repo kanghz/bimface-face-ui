03b86c518b26feee9bfc99ccb46732fcc3afb5b4
Jerry <liangwq@glodon.com>
2018-10-09 14:55:51 +0800

    update

--- src/components/marker3D/index.js
+++ src/components/marker3D/index.js
@@ -8,7 +8,21 @@ let Marker3D = () => {
 }
 
 Marker3DConstructor.prototype.open = function (options) {
-  console.log(options);
+  var markerConfig = new Glodon.Bimface.Plugins.Marker3D.Marker3DContainerConfig();
+  markerConfig.viewer = options.viewer;
+  var marker = new Glodon.Bimface.Plugins.Marker3D.Marker3DContainer(markerConfig);
+  options.viewer.addEventListener(Glodon.Bimface.Application.WebApplication3DEvent.ComponentsSelectionChanged,function(componentData) {
+    if(componentData && componentData.objectId){
+      var marker3dConfig = new Glodon.Bimface.Plugins.Marker3D.Marker3DConfig();
+      marker3dConfig.src = options.url;
+      marker3dConfig.worldPosition = componentData.worldPosition;
+      var marker3d = new Glodon.Bimface.Plugins.Marker3D.Marker3D(marker3dConfig);
+      marker3d.onClick(function(item){
+        console.log(item.id);
+      })
+      marker.addItem(marker3d);
+    }
+  });
 }
 
 export default Marker3D()
